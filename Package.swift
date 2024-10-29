5a1f54427aedc51531f6c1ace4eaed376bae4dff
Jerry_hoyoung <llghdud921@naver.com>
2024-10-29 17:39:11 +0900

    target name 변경

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ import PackageDescription
 /// A precompiled XCFramework of the lottie-ios repo that was compiled with Xcode 15.2 / Swift 5.9.
 /// This XCFramework can be used by Xcode 15.0 and later.
 let lottieXCFramework = Target.binaryTarget(
-  name: "Lottie",
+  name: "SlingLottie",
   url: "https://github.com/airbnb/lottie-ios/releases/download/4.5.0/Lottie.xcframework.zip",
   checksum: "ea474237ceca92ae12ee276c167f733f75567e9ca144dfa92678dfe5073d77b1")
 
 let package = Package(
   name: "SlingLottie",
   platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("13.0"), .custom("visionOS", versionString: "1.0")],
-  products: [.library(name: "SlingLottie", targets: ["Lottie", "_LottieStub"])],
+  products: [.library(name: "SlingLottie", targets: ["SlingLottie", "_LottieStub"])],
   targets: [
     lottieXCFramework,
     
@@ -22,10 +22,11 @@ let package = Package(
     // ran on a physical device. As a workaround, we can include a stub target
     // with at least one source file.
     // https://github.com/apple/swift-package-manager/issues/6069
-    .target(name: "_LottieStub"),
+        .target(name: "_LottieStub"),
     
-    .testTarget(
-      name: "LottieTests",
-      dependencies: ["Lottie"],
-      path: "Tests")
-  ])
+//        .testTarget(
+//            name: "LottieTests",
+//            dependencies: ["SlingLottie"],
+//            path: "Tests")
+  ]
+)
